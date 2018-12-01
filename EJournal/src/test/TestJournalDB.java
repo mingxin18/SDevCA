0bde3b45dcb782e0befec62f7ff9e869e5b3e2aa
mingxin18 <43670128+mingxin18@users.noreply.github.com>
2018-12-01 19:31:56 +0000

    Try to make a big list of menu
    Trying to add the code of separating the menu lists to three lists of Journal , Viewing , Add/Remove

--- EJournal/src/test/TestJournalDB.java
+++ EJournal/src/test/TestJournalDB.java
@@ -32,146 +32,230 @@ public class TestJournalDB {
         Scanner in = new Scanner(System.in);
 
         while (true) {
-            System.out.println("TEST MENU");
-            System.out.println("Press 1 to add a manuscript.");
-            System.out.println("Press 2 to assign a manuscript to reviewers.");
-            System.out.println("Press 3 to submit a review of a manuscript.");
-            System.out.println("Press 4 to see reviews of a manuscript.");
-            System.out.println("Press 5 to accept/reject manuscript.");//Nested menu in here maybe?
-            System.out.println("Press 6 to add a journal issue.");
-            System.out.println("Press 7 to schedule a manuscript for publication.");//adding manuscript to journal List
-            System.out.println("Press 8 to publish an issue.");
-            //Searching and viewing. Nested menu?
-//            System.out.println("Press 9 to view all authors of a manuscript.");
-//            System.out.println("Press 10 to view all manuscripts in a published journal issue.");
-//            System.out.println("Press 11 to see all manuscripts submitted by an author.");
-//            System.out.println("Press 12 to search for a key word.");//Might be difficult to implement, check later.
-//            System.out.println("Press 13 to see all details on a manuscript.");
-//            System.out.println("Press 14 to see all details on an author.");
-//            System.out.println("Press 15 to see all details on a reviewer.");
-//            System.out.println("Press 16 to see all details on a journal issue.");
-            
-            //Admin, adding and removing entities. Nested Menu?
-//            System.out.println("Press x to add an author.");
-//            System.out.println("Press x to add a reviewer.");
-//            System.out.println("Press x to add an affiliate.");
-//            System.out.println("Press x to remove an author.");
-//            System.out.println("Press x to remove a reviewer.");
-//            System.out.println("Press x to remove an affiliate.");
-            
-            //Exit
-            System.out.println("Press 99 to exit.");
+            System.out.println("Press 1 to edit Journal.");
+            System.out.println("Press 2 to search and view information.");
+            System.out.println("Press 3 to add and remove information.");
+            System.out.println("Press 4 to exit.");
 
-            int choice = in.nextInt();
+            int menu = in.nextInt();
             in.nextLine();
-            switch (choice) {
+            switch (menu) {
                 case 1:
-                    System.out.println("Please enter title of manuscript");
-                    String title = in.nextLine();
-                    System.out.println("Please enter how many authors have contributed to this manuscript: ");
-                    int authNum = in.nextInt();
-                    in.nextLine();
-                    for (int i = 0; i < authNum; i++) {
-                        System.out.println("Please enter ID of author " + (i + 1) + " of manuscript: ");
-                        int authID = in.nextInt();
+                    while (true) {
+                        System.out.println("TEST MENU");
+                        System.out.println("Press 1 to add a manuscript.");
+                        System.out.println("Press 2 to assign a manuscript to reviewers.");
+                        System.out.println("Press 3 to submit a review of a manuscript.");
+                        System.out.println("Press 4 to see reviews of a manuscript.");
+                        System.out.println("Press 5 to accept/reject manuscript.");//Nested menu in here maybe?
+                        System.out.println("Press 6 to add a journal issue.");
+                        System.out.println("Press 7 to schedule a manuscript for publication.");//adding manuscript to journal List
+                        System.out.println("Press 8 to publish an issue.");
+
+                        System.out.println("Press 9 to exit.");
+
+                        int choice = in.nextInt();
                         in.nextLine();
-//                        po.addManuscript(authID,title,currentDate);
+                        switch (choice) {
+                            case 1:
+                                System.out.println("Please enter title of manuscript");
+                                String title = in.nextLine();
+                                System.out.println("Please enter how many authors have contributed to this manuscript: ");
+                                int authNum = in.nextInt();
+                                in.nextLine();
+                                for (int i = 0; i < authNum; i++) {
+                                    System.out.println("Please enter ID of author " + (i + 1) + " of manuscript: ");
+                                    int authID = in.nextInt();
+                                    in.nextLine();
+//                                  po.addManuscript(authID,title,currentDate);
+                                }
+                                //Change to 'received'
+                                break;
+                            case 2:
+                                System.out.println("Please enter the ID of the manuscript to review");
+                                int manuID = in.nextInt();
+                                in.nextLine();
+                                System.out.println("Please enter the number of reviewers to review the manuscript");
+                                int revNum = in.nextInt();
+                                for (int i = 0; i < revNum; i++) {
+                                    System.out.println("Please enter ID of reviewer " + (i + 1) + " of manuscript: ");
+                                    int revID = in.nextInt();
+                                    in.nextLine();
+//                                  po.reviewManuscript(revID, manuID, currentDate);
+
+                                }
+                                //Change to 'under review'
+                                break;
+                            case 3:
+                                System.out.println("Please enter your reviewer ID: ");
+                                int revID = in.nextInt();
+                                in.nextLine();
+                                System.out.println("Please enter ID of manuscript you are submitting a review for: ");
+                                manuID = in.nextInt();
+                                in.nextLine();
+
+                                int revAppropriate;
+                                do {
+                                    System.out.println("Please enter rating for appropriateness (1-10): ");
+                                    revAppropriate = in.nextInt();
+                                    in.nextLine();
+                                    if (revAppropriate < 1 || revAppropriate > 10) {
+                                        System.out.println("Rating must be between 1 and 10 inclusive.");
+                                    }
+                                } while (revAppropriate < 1 || revAppropriate > 10);
+
+                                int revClarity;
+                                do {
+                                    System.out.println("Please enter rating for clarity (1-10): ");
+                                    revClarity = in.nextInt();
+                                    in.nextLine();
+                                    if (revClarity < 1 || revClarity > 10) {
+                                        System.out.println("Rating must be between 1 and 10 inclusive.");
+                                    }
+                                } while (revClarity < 1 || revClarity > 10);
+
+                                int revMethod;
+                                do {
+                                    System.out.println("Please enter rating for method (1-10): ");
+                                    revMethod = in.nextInt();
+                                    in.nextLine();
+                                    if (revMethod < 1 || revMethod > 10) {
+                                        System.out.println("Rating must be between 1 and 10 inclusive.");
+                                    }
+                                } while (revMethod < 1 || revMethod > 10);
+
+                                int revContribution;
+                                do {
+                                    System.out.println("Please enter rating for contribution (1-10): ");
+                                    revContribution = in.nextInt();
+                                    in.nextLine();
+                                    if (revContribution < 1 || revContribution > 10) {
+                                        System.out.println("Rating must be between 1 and 10 inclusive.");
+                                    }
+                                } while (revContribution < 1 || revContribution > 10);
+
+                                System.out.println("Please enter recommendation (accept/reject): ");
+                                String revRecommendation = in.next();
+                                in.nextLine();
+//                               po.addReview(revID,manuID,revAppropriate,revClarity,revMethod,revContribution,revRecommendation,currentDate);
+                                break;
+                            case 4:
+                                System.out.println("Please enter ID of manuscript for which you want to view reviews: ");
+                                manuID = in.nextInt();
+                                in.nextLine();
+//                               po.viewReviews(manuID);
+                                break;
+                            case 5://Maybe nested switch to pick accept/reject.
+                                System.out.println("Please enter ID of manuscript you wish to accept/reject: ");
+                                manuID = in.nextInt();
+                                in.nextLine();
+                                System.out.println("Please enter if you wish to accept or reject this manuscript (accept/reject): ");
+                                String x = in.next();
+                                in.nextLine();
+//                               po.accept(x,currentDate);
+                                //Should change to 'accepted' or 'rejected'
+                                break;
+                            case 9:
+                                jo.closeDB();
+                                System.exit(0);
+                            default:
+                                System.out.println("Invalid option entered.");
+                                break;
+                        }
                     }
-                    //Change to 'received'
+                    
                     break;
-                case 2:
-                    System.out.println("Please enter the ID of the manuscript to review");
-                    int manuID = in.nextInt();
-                    in.nextLine();
-                    System.out.println("Please enter the number of reviewers to review the manuscript");
-                    int revNum = in.nextInt();
-                    for (int i = 0; i < revNum; i++) {
-                        System.out.println("Please enter ID of reviewer " + (i + 1) + " of manuscript: ");
-                        int revID = in.nextInt();
-                        in.nextLine();
-//                        po.reviewManuscript(revID, manuID, currentDate);
+
+                    
+            
+                case 2://ERROR !!! Error Message saying that : orphaned case
+                    while (true) {
+                
+                        System.out.println("Press 1 to view all authors of a manuscript.");
+                        System.out.println("Press 2 to view all manuscripts in a published journal issue.");
+                        System.out.println("Press 3 to see all manuscripts submitted by an author.");
+                        System.out.println("Press 4 to search for a key word.");//Might be difficult to implement, check later.
+                        System.out.println("Press 5 to see all details on a manuscript.");
+                        System.out.println("Press 6 to see all details on an author.");
+                        System.out.println("Press 7 to see all details on a reviewer.");
+                        System.out.println("Press 8 to see all details on a journal issue.");
+                        System.out.println("Press 9 to exit.");
+
+                            int choice = in.nextInt();
+                            in.nextLine();
+                            switch (choice) {
+                                case 1:
+                        
+                                    break;
+                                case 2:
+                        
+                                    break;
+                                case 3:
+                        
+                                    break;
+                                case 4:
+                        
+                                    break;
+                                case 5:
+                        
+                                    break;
+                                case 9:
+                                    jo.closeDB();
+                                    System.exit(0);
+                                default:
+                                   System.out.println("Invalid option entered.");
+                                   break;
+                            }
 
                     }
-                    //Change to 'under review'
                     break;
                 case 3:
-                    System.out.println("Please enter your reviewer ID: ");
-                    int revID = in.nextInt();
-                    in.nextLine();
-                    System.out.println("Please enter ID of manuscript you are submitting a review for: ");
-                    manuID = in.nextInt();
-                    in.nextLine();
-
-                    int revAppropriate;
-                    do {
-                        System.out.println("Please enter rating for appropriateness (1-10): ");
-                        revAppropriate = in.nextInt();
+                    while (true) {  
+                        System.out.println("Press 1 to add an author.");           
+                        System.out.println("Press 2 to add a reviewer.");
+                        System.out.println("Press 3 to add an affiliate.");
+                        System.out.println("Press 4 to remove an author.");
+                        System.out.println("Press 5 to remove a reviewer.");
+                        System.out.println("Press 6 to remove an affiliate.");
+                        System.out.println("Press 7 to exit.");
+                    
+                        int choice = in.nextInt();
                         in.nextLine();
-                        if (revAppropriate < 1 || revAppropriate > 10) {
-                            System.out.println("Rating must be between 1 and 10 inclusive.");
+                        switch (choice) {
+                            case 1:
+                        
+                                break;
+                            case 2:
+                        
+                                break;
+                            case 3:
+                        
+                                break;
+                            case 4:
+                        
+                                break;
+                            case 5:
+                        
+                                break;
+                            case 7:
+                                jo.closeDB();
+                                System.exit(0);
+                            default:
+                                System.out.println("Invalid option entered.");
+                                break;
                         }
-                    } while (revAppropriate < 1 || revAppropriate > 10);
-
-                    int revClarity;
-                    do {
-                        System.out.println("Please enter rating for clarity (1-10): ");
-                        revClarity = in.nextInt();
-                        in.nextLine();
-                        if (revClarity < 1 || revClarity > 10) {
-                            System.out.println("Rating must be between 1 and 10 inclusive.");
-                        }
-                    } while (revClarity < 1 || revClarity > 10);
-
-                    int revMethod;
-                    do {
-                        System.out.println("Please enter rating for method (1-10): ");
-                        revMethod = in.nextInt();
-                        in.nextLine();
-                        if (revMethod < 1 || revMethod > 10) {
-                            System.out.println("Rating must be between 1 and 10 inclusive.");
-                        }
-                    } while (revMethod < 1 || revMethod > 10);
-
-                    int revContribution;
-                    do {
-                        System.out.println("Please enter rating for contribution (1-10): ");
-                        revContribution = in.nextInt();
-                        in.nextLine();
-                        if (revContribution < 1 || revContribution > 10) {
-                            System.out.println("Rating must be between 1 and 10 inclusive.");
-                        }
-                    } while (revContribution < 1 || revContribution > 10);
-
-                    System.out.println("Please enter recommendation (accept/reject): ");
-                    String revRecommendation = in.next();
-                    in.nextLine();
-//                    po.addReview(revID,manuID,revAppropriate,revClarity,revMethod,revContribution,revRecommendation,currentDate);
+                    }
                     break;
                 case 4:
-                    System.out.println("Please enter ID of manuscript for which you want to view reviews: ");
-                    manuID = in.nextInt();
-                    in.nextLine();
-//                    po.viewReviews(manuID);
-                    break;
-                case 5://Maybe nested switch to pick accept/reject.
-                    System.out.println("Please enter ID of manuscript you wish to accept/reject: ");
-                    manuID = in.nextInt();
-                    in.nextLine();
-                    System.out.println("Please enter if you wish to accept or reject this manuscript (accept/reject): ");
-                    String x = in.next();
-                    in.nextLine();
-//                    po.accept(x,currentDate);
-                    //Should change to 'accepted' or 'rejected'
-                    break;
-                case 99:
                     jo.closeDB();
                     System.exit(0);
                 default:
                     System.out.println("Invalid option entered.");
                     break;
             }
-
         }
+        
+        
     }
 
 }
